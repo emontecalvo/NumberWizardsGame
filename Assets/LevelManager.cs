9d534a6f52dcb4789cde1858492a17b595a66683
emontecalvo <emilymontecalvo@gmail.com>
2017-10-04 14:23:03 -0700

    initial commit, changing console Unity game into a UI game

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level Load requested for : " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit Request requested");
+		Application.Quit ();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+
+	public int maxGuessesAllowed;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame() {
+		maxGuessesAllowed = 5;
+		max = 1000;
+		min = 1;
+		NextGuess();
+
+	}
+
+	void NextGuess() {
+//		guess = (max + min) / 2;
+		guess = Random.Range(min, max+1);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess ();
+	}
+
+
+
+}
